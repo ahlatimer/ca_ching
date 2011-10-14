e2807a6bfd42dcda37e4d30635898b59f5f450fc
Andrew Latimer <andrew@elpasoera.com>
2011-10-13 17:44:04 -0700

    More specs for WriteThrough. AR#save! cache support.

--- lib/ca_ching/write_through.rb
+++ lib/ca_ching/write_through.rb
@@ -12,11 +12,19 @@ module CaChing
         CaChing.cache.update(self)
         save_without_cache
       end
+      
+      def save_with_cache!
+        return save_without_cache! if CaChing.cache.nil? || CaChing.disabled?
+        
+        CaChing.cache.update(self)
+        save_without_cache!
+      end
     end
     
     included do |base|
       base.class_eval do
         alias_method_chain :save, :cache
+        alias_method_chain :save!, :cache
       end
     end
   end
--- spec/ca_ching/write_through_spec.rb
+++ spec/ca_ching/write_through_spec.rb
@@ -11,15 +11,41 @@ module CaChing
       @cache.clear!
     end
     
-    it 'updates the cache for a cached object' do
+    it 'updates the cache for a cached object with AR#save' do
       ar = Person.where(:id => 1)
       query = CaChing::Query::Abstract.new(ar)
       @cache.insert(ar.to_a_without_cache, :for => query)
       
-      @person.name = @person.name.reverse
+      name = @person.name.reverse
+      
+      @person.name = name
       @person.save
       
-      @cache.find(CaChing::Query::Abstract.new(ar)).first.name.should == @person.name
+      @cache.find(CaChing::Query::Abstract.new(ar)).first.name.should == name
+    end
+
+    it 'updates the cache for a cached object with AR#save!' do
+      ar = Person.where(:id => 1)
+      query = CaChing::Query::Abstract.new(ar)
+      @cache.insert(ar.to_a_without_cache, :for => query)
+      
+      name = @person.name.reverse
+      
+      @person.name = name
+      @person.save!
+      
+      @cache.find(CaChing::Query::Abstract.new(ar)).first.name.should == name
+    end
+    
+    it 'updates the cache for a cached object with AR#update_attributes' do
+      ar = Person.where(:id => 1)
+      query = CaChing::Query::Abstract.new(ar)
+      @cache.insert(ar.to_a_without_cache, :for => query)
+
+      name = @person.name.reverse
+      @person.update_attributes(:name => name)
+      
+      @cache.find(CaChing::Query::Abstract.new(ar)).first.name.should == name
     end
   end
 end
