fe2aa9daa281ed7231757803cc836cef102d6db8
Andrew Latimer <andrew@elpasoera.com>
2011-10-19 14:06:28 -0700

    Destroy from cache on AR#destroy. Needs a spec.

--- lib/ca_ching/write_through.rb
+++ lib/ca_ching/write_through.rb
@@ -19,12 +19,20 @@ module CaChing
         CaChing.cache.update(self)
         save_without_cache!
       end
+      
+      def destroy_with_cache(*)
+        return destroy_without_cache if CaChing.cache.nil? || CaChing.disabled?
+        
+        CaChing.cache.destroy(self)
+        destroy_without_cache
+      end
     end
     
     included do |base|
       base.class_eval do
         alias_method_chain :save, :cache
         alias_method_chain :save!, :cache
+        alias_method_chain :destroy, :cache
       end
     end
   end
